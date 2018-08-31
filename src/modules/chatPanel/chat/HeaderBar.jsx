f60817abc68691d589c23d9a763776d4f1deb471
moyui <gxy1510@gmail.com>
2018-09-01 03:26:22 +0800

    dev:客户端完成

---
+++ src/modules/chatPanel/chat/HeaderBar.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import immutable from 'immutable';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+import IconButton from '@/components/IconButton';
+import Message from '@/components/Message';
+
+class HeaderBar extends Component {
+    static handleShareGroup() {
+        Message.success('已复制邀请信息到粘贴板, 去邀请其它人加群吧');
+    }
+    render() {
+        const { linkman, onShowInfo, isLogin } = this.props;
+        if (!linkman) {
+            return <div />;
+        }
+        return (
+            <div className="chat-headerBar">
+                <h2>{linkman && linkman.get('name')}</h2>
+                {
+                    isLogin ?
+                        <div>
+                            {
+                                linkman.get('type') === 'group' ?
+                                    <CopyToClipboard text={`invite::${linkman.get('name')}`}>
+                                        <IconButton width={40} height={40} icon="share" iconSize={24} onClick={HeaderBar.handleShareGroup} />
+                                    </CopyToClipboard>
+
+                                    :
+                                    null
+                            }
+                            <IconButton width={40} height={40} icon="gongneng" iconSize={24} onClick={onShowInfo} />
+                        </div>
+                        :
+                        null
+                }
+            </div>
+        );
+    }
+}
+
+export default connect((state) => {
+    const focus = state.get('focus');
+    const linkmans = state.getIn(['user', 'linkmans']) || immutable.fromJS([]);
+    const linkman = linkmans.find(l => l.get('_id') === focus);
+    return {
+        isLogin: !!state.getIn(['user', '_id']),
+        linkman,
+    };
+})(HeaderBar);
---
+++ src/modules/chatPanel/chat/MessageList.jsx
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import immutable from 'immutable';
+import PropTypes from 'prop-types';
+import autobind from 'autobind-decorator';
+
+import fetch from 'utils/fetch';
+import action from '@/state/action';
+import Message from './Message';
+
+function noop() {}
+
+class MessageList extends Component {
+    constructor(...args) {
+        super(...args);
+        this.isFetching = false;
+    }
+    @autobind
+    async handleScroll(e) {
+        // Don't know why the code-view dialog will also trigger when scrolling
+        if (e.target !== this.$list) {
+            return;
+        }
+        if (this.isFetching) {
+            return;
+        }
+
+        const { focus, messages, self } = this.props;
+        const $div = e.target;
+        //拉倒顶端的时候查询历史记录
+        if ($div.scrollTop === 0 && $div.scrollHeight > $div.clientHeight) {
+            this.isFetching = true;
+            let err = null;
+            let result = null;
+            if (self) {
+                [err, result] = await fetch('getLinkmanHistoryMessages', { linkmanId: focus, existCount: messages.size });
+            } else {
+                [err, result] = await fetch('getDefalutGroupHistoryMessages', { existCount: messages.size });
+            }
+            if (!err) {
+                action.addLinkmanMessages(focus, result);
+            }
+            this.isFetching = false;
+        }
+    }
+    renderMessage(message) {
+        const { self } = this.props;
+        const props = {
+            key: message.get('_id'),
+            avatar: message.getIn(['from', 'avatar']),
+            nickname: message.getIn(['from', 'username']),
+            time: new Date(message.get('createTime')),
+            type: message.get('type'),
+            content: message.get('content'),
+            isSelf: self === message.getIn(['from', '_id']),
+            tag: message.getIn(['from', 'tag']),
+            openUserInfoDialog: noop,
+            shouldScroll: this.$list.scrollHeight === this.$list.clientHeight || this.$list.scrollTop === 0 || this.$list.scrollTop > this.$list.scrollHeight - this.$list.clientHeight * 2,
+        };
+        if (props.type === 'image') {
+            props.loading = message.get('loading');
+            props.percent = message.get('percent');
+        }
+        if (!props.isSelf && self) {
+            props.openUserInfoDialog = this.props.showUserInfoDialog.bind(this, message.get('from').toJS());
+        }
+        return (
+            <Message {...props} />
+        );
+    }
+    render() {
+        const { messages } = this.props;
+        return (
+            <div className="chat-messageList" onScroll={this.handleScroll} ref={i => this.$list = i}>
+                {
+                    messages.map(message => (
+                        this.renderMessage(message)
+                    ))
+                }
+            </div>
+        );
+    }
+}
+
+export default connect((state) => {
+    const isLogin = !!state.getIn(['user', '_id']);
+    let messages = immutable.List();
+    if (!isLogin) {
+        const defaultGroupMessages = state.getIn(['user', 'linkmans', 0, 'messages']);
+        return {
+            self: '',
+            messages: defaultGroupMessages || messages,
+        };
+    }
+
+    const self = state.getIn(['user', '_id']);
+    const focus = state.get('focus');
+    const linkman = state.getIn(['user', 'linkmans']).find(g => g.get('_id') === focus);
+    if (linkman) {
+        messages = linkman.get('messages');
+    }
+
+    return {
+        self,
+        focus,
+        messages,
+    };
+})(MessageList);
